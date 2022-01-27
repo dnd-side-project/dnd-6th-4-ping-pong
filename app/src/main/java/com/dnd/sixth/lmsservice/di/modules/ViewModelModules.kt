51869d4222803f0db35a84bf8f84cbd0d83e6ade
jeongminko <terrantrace@gmail.com>
2022-01-28 06:17:41 +0900

    Design(Login) : Make layout of MakeClassActivity
    -creating layout activity_login.xml
    -creating MakeClassActivity, MakeClassViewModel
    close : #17

--- app/src/main/java/com/dnd/sixth/lmsservice/di/modules/ViewModelModules.kt
+++ app/src/main/java/com/dnd/sixth/lmsservice/di/modules/ViewModelModules.kt
@@ -1,11 +1,13 @@
 package com.dnd.sixth.lmsservice.di.modules
 
 import com.dnd.sixth.lmsservice.presentation.home.HomeViewModel
+import com.dnd.sixth.lmsservice.presentation.login.LoginViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val mainViewModelModule = module {
     viewModel {
         HomeViewModel()
+        LoginViewModel()
     }
 }
