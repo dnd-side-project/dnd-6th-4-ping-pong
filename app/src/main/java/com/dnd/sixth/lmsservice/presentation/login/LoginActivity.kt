911a86b351c40028001f66d1af55a0d4aef66251
tmdgh1592 <tmdgh1592@naver.com>
2022-01-28 20:05:49 +0900

    Design(calendar): "Design calendar activity layout"
    - adding Tooltip library.
    - create calendar fragment, viewModel, layout.
    - adding material calendar library for using temporarily.
    - merged the viewModel module into one di variable.
    
    close : #27

--- app/src/main/java/com/dnd/sixth/lmsservice/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/dnd/sixth/lmsservice/presentation/login/LoginActivity.kt
@@ -1,11 +1,8 @@
 package com.dnd.sixth.lmsservice.presentation.login
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.dnd.sixth.lmsservice.R
 import com.dnd.sixth.lmsservice.databinding.ActivityLoginBinding
 import com.dnd.sixth.lmsservice.presentation.base.BaseActivity
-import com.dnd.sixth.lmsservice.presentation.home.HomeViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>() {
@@ -16,8 +13,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>() {
 
     // 액티비티 초기화 메서드
     override fun initActivity() {
-
-        with(binding){
+        with(binding) {
             viewModel = this@LoginActivity.viewModel
         }
 
