09c3c8bd5ecdb8fabd26be11a93588206a91abfa
jeongminko <terrantrace@gmail.com>
2022-01-31 22:17:17 +0900

    Design(communication) : Delete layout and activity,fragment,adapter about Communication 1. 디자인 변경으로 인한 소통하기 부분 모두 삭제 close : #56

--- app/src/main/java/com/dnd/sixth/lmsservice/presentation/communication/CommunicationMainViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.dnd.sixth.lmsservice.presentation.communication
-
-import com.dnd.sixth.lmsservice.presentation.base.BaseViewModel
-
-class CommunicationMainViewModel : BaseViewModel(){
-}
