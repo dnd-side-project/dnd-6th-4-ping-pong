76f01823e7c475f65fc56b62ba2e993602528c82
tmdgh1592 <tmdgh1592@naver.com>
2022-02-22 13:24:39 +0900

    Feature(date convert): "Convert date to day of week"
    - Make function of converting date to day of week like '2022-02-22 to 화'
    
    close : #127

--- app/src/main/java/com/dnd/sixth/lmsservice/presentation/utility/DateConverter.kt
+++ app/src/main/java/com/dnd/sixth/lmsservice/presentation/utility/DateConverter.kt
@@ -1,12 +1,21 @@
 package com.dnd.sixth.lmsservice.presentation.utility
 
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.Calendar.DAY_OF_WEEK
+
 class DateConverter {
 
+    /*
+    *  Calendar 객체의 DAY_OF_WEEK의 Value값으로 요일을 구함 (일 ~ 토)
+    *  @param dayOfWeek : Calendar 객체의 DAY_OF_WEEK(Key)의 value
+    * */
     fun getDayOfWeek(dayOfWeek: Int): String {
         // 요일을 담을 String 변수
         var dayOfWeekText = ""
 
-        when(dayOfWeek) {
+        when (dayOfWeek) {
             1 -> dayOfWeekText = "일"
             2 -> dayOfWeekText = "월"
             3 -> dayOfWeekText = "화"
@@ -17,5 +26,20 @@ class DateConverter {
         }
 
         return dayOfWeekText
+    }
+
+
+    /*
+    *  특정 날짜에 대하여 요일을 구함 (일 ~ 토)
+    *  @param date : 입력받을 날짜의 String 형태 (ex. 2022-02-22)
+    * */
+    @SuppressLint("SimpleDateFormat")
+    fun getDayOfWeek(date: String): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+        val parsedDate = dateFormat.parse(date)!!
+        val calendar = Calendar.getInstance()
+
+        calendar.time = parsedDate // 캘린더 객체에 Date 지정
+        return getDayOfWeek(calendar[DAY_OF_WEEK]) // Calendar의 요일 숫자를 요일 텍스트로 반환
     }
 }
