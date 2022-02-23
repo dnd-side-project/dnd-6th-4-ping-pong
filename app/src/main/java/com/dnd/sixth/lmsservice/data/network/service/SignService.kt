a75f144d2be201a33cb27201543a339fc13bae04
kojeongmin <terrnatrace@gmail.com>
2022-02-23 16:20:56 +0900

    Feature(Login) : add Login Func and Save Info at SharedPreference 1. 로그인 버튼 누르고 가입된 이메일만 입력하면 로그인 가능 2. 로그인 하는 시점에 유저 정보를 요청하여 필요한 정보들은 로컬에 저장 close : #134

--- app/src/main/java/com/dnd/sixth/lmsservice/data/network/service/SignService.kt
+++ app/src/main/java/com/dnd/sixth/lmsservice/data/network/service/SignService.kt
@@ -1,12 +1,12 @@
 package com.dnd.sixth.lmsservice.data.network.service
 
 import com.dnd.sixth.lmsservice.data.response.UserCreateDto
-import com.dnd.sixth.lmsservice.data.response.User
-import com.dnd.sixth.lmsservice.data.response.UserListResponse
+import com.dnd.sixth.lmsservice.data.response.UserResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface SignService {
 
@@ -16,10 +16,10 @@ interface SignService {
 
     //화원가입
     @POST("/user")
-    fun signUp(@Body post : UserCreateDto) : Call<User>
+    fun signUp(@Body post : UserCreateDto) : Call<UserResponse>
 
-    //임시로 사용
+    //로그인 시점에 저장할 유저의 정보 저장
     //유저 정보 받아오기 (원래 전체 유저 받아오는 요청)
-    @GET("/user/all")
-    fun getUserInfo() : Call<UserListResponse>
+    @GET("/user/{email}")
+    fun getUserInfo(@Path("email") email : String) : Call<UserResponse>
 }
--- app/src/main/java/com/dnd/sixth/lmsservice/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/dnd/sixth/lmsservice/presentation/login/LoginViewModel.kt
@@ -2,9 +2,9 @@ package com.dnd.sixth.lmsservice.presentation.login
 
 import androidx.lifecycle.viewModelScope
 import com.dnd.sixth.lmsservice.data.preference.PreferenceManager
+import com.dnd.sixth.lmsservice.data.response.UserResponse
 import com.dnd.sixth.lmsservice.presentation.base.BaseViewModel
-import com.dnd.sixth.lmsservice.presentation.utility.SAVED_ID_KEY
-import com.dnd.sixth.lmsservice.presentation.utility.SAVED_PW_KEY
+import com.dnd.sixth.lmsservice.presentation.utility.*
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
@@ -18,31 +18,28 @@ class LoginViewModel(
     }
 
     //자동 로그인 데이터 저장 & 로그인
-    fun saveLoginInfo(userId : String, userPw : String){
+    fun saveLoginInfo(userInfo : UserResponse){
 
         //if (autoSave){
 
         //SharedPreference에 id와 pw저장
         with(preferenceManager){
 
-            setString(SAVED_ID_KEY,userId)
-            setString(SAVED_PW_KEY,userPw)
-            //setString(Saved_NAME_KEY,name)
-            //setString(Saved_ROLE_KEY,role)
-            //setString(Saved_PHONE_NUMBER_KEY, phone_number)
+            setString(SAVED_ID_KEY,userInfo.email)
+            setString(SAVED_PW_KEY,userInfo.password)
+            setString(Saved_NAME_KEY,userInfo.email)
+            setString(Saved_ROLE_KEY,userInfo.email)
+            setString(Saved_PHONE_NUMBER_KEY, userInfo.email)
             //setString(Saved_UID_KEY, uid)
             //TODO 토큰 저장
 
 
         }
 
-        //로그인
-        login()
+
     }
 
-    fun login(){
-        //api명세서가 업데이트 되는대로 구현 예정
-    }
+
 
 
 
