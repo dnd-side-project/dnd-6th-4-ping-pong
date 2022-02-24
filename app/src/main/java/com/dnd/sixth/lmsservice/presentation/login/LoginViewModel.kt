8d7a61cfc85c0d442f77c9f3928f3716f628e9a7
eigen98 <terrnatrace@gmail.com>
2022-02-24 11:51:37 +0900

    Feature(Login) : Add autoLogin 1. 체크한 상태로 로그인 시 로컬에 이메일 저장되는 형태 close : #146

--- app/src/main/java/com/dnd/sixth/lmsservice/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/dnd/sixth/lmsservice/presentation/login/LoginViewModel.kt
@@ -1,15 +1,21 @@
 package com.dnd.sixth.lmsservice.presentation.login
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.dnd.sixth.lmsservice.data.network.api.SignApi
 import com.dnd.sixth.lmsservice.data.preference.PreferenceManager
 import com.dnd.sixth.lmsservice.data.response.UserResponse
 import com.dnd.sixth.lmsservice.presentation.base.BaseViewModel
 import com.dnd.sixth.lmsservice.presentation.utility.*
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginViewModel(
-    private val preferenceManager: PreferenceManager,
+    private val preferenceManager: PreferenceManager
 ) : BaseViewModel() {
 
     // 뷰모델 스코프 :  뷰모델이 onClear -> 코루틴도 모두 중단
@@ -17,10 +23,30 @@ class LoginViewModel(
 
     }
 
-    //자동 로그인 데이터 저장 & 로그인
+    //api
+    var LoginApi : SignApi = SignApi()
+
+    //자동로그인 여부
+    var saveDataCheck : MutableLiveData<Boolean> = MutableLiveData<Boolean>()
+
+
+
+    fun init() {
+        saveDataCheck.value = false
+
+    }
+
+
+
+
+    //자동 로그인 데이터 저장
     fun saveLoginInfo(userInfo : UserResponse){
 
-        //if (autoSave){
+        //로그인 상태 저장하시겠습니까
+        if (saveDataCheck.value!!){
+            preferenceManager.setString(AUTO_EMAIL_KEY,userInfo.email)
+            preferenceManager.setString(AUTO_PASSWORD_KEY,userInfo.password)
+        }
 
         //SharedPreference에 id와 pw저장
         with(preferenceManager){
@@ -38,6 +64,18 @@ class LoginViewModel(
 
 
     }
+
+    fun getStringFromViewModel(key : String) : String?{
+        return preferenceManager.getString(key)
+    }
+
+            //로그인 요청 api 업데이트 되었을 때 구현 예정
+    fun autoLogin(){
+        var email = preferenceManager.getString(AUTO_EMAIL_KEY).orEmpty()
+        var password = preferenceManager.getString(AUTO_PASSWORD_KEY).orEmpty()
+    }
+
+
 
 
 
