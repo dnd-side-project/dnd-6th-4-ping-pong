309e74db78ff73d11d38ce8b0c2e68dd702d868b
tmdgh1592 <tmdgh1592@naver.com>
2022-02-25 01:09:04 +0900

    Feature(change password): "Implement a password change"
    - Implemented the password change function in the setting screen
    
    related to : #162

--- app/src/main/java/com/dnd/sixth/lmsservice/domain/useCase/user/ChangeUserNameUseCase.kt
+++ app/src/main/java/com/dnd/sixth/lmsservice/domain/useCase/user/ChangeUserNameUseCase.kt
@@ -3,7 +3,7 @@ package com.dnd.sixth.lmsservice.domain.usecase.user
 import com.dnd.sixth.lmsservice.domain.repository.UserRepository
 
 class ChangeUserNameUseCase(private val userRepository: UserRepository) {
-    suspend operator fun invoke(uid: Number, changeName: String): Int {
-        return userRepository.changeUserName(uid, changeName)
+    suspend operator fun invoke(uid: Number, newName: String): Int {
+        return userRepository.changeUserName(uid, newName)
     }
 }
